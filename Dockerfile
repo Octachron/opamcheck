09ea67dd9dffc9359cb4959ab5b4203fa8f90e0d
Florian Angeletti <florian.angeletti@inria.fr>
2019-08-14 18:32:26 +0200

    Improved dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,26 @@
-from opam2:ubuntu
+from ocaml/opam2:ubuntu as STAGE_1
+COPY --chown=opam ./external_deps_install.sh /
+RUN bash /external_deps_install.sh
 
+from STAGE_1 as STAGE_2
+USER opam
+RUN eval $(opam env) && opam install minisat opam-file-format dune js_of_ocaml-lwt
+
+from STAGE_2 as STAGE_3
+COPY --chown=opam . /app
+USER opam
 WORKDIR /app
-Copy . /app
+Copy --chown=opam . /app
+RUN eval $(opam env) && make
+RUN bash init.sh as stage_3
 
-RUN bash conf_install.sh
 
-RUN adduser opamcheck
-USER myuser
+from STAGE_3 as STAGE_4
+USER opam
+RUN mkdir /log
+COPY --chown=opam ./params /app
+COPY --chown=opam ./launch.sh /app
+RUN chmod u+x launch.sh
+CMD "/app/launch.sh"
 
-Run bash init.sh
 
-CMD ["bash", "launch.sh"]
--- launch.sh
+++ launch.sh
@@ -1,1 +1,19 @@
-OPCSANDBOX="/app/sandbox" PATH="${PATH}:." ./src/launch --retries=2 $(cat params)
+echo Launching opamcheck for $(cat params)
+
+PATH="${PATH}:."
+PATH=${PATH##*.opam}
+PATH=${PATH#*:}
+PARAMS=$(cat params)
+OPCSANDBOX=/app/sandbox
+export OPCSANDBOX=$OPCSANDBOX
+set $PARAMS
+unset OPAMSWITCH
+printf "<p>opamcheck launched on %s<br>" "$(date -u +"%F %T UTC")"
+printf "<p>opamcheck launched on %s<br>" "$(date -u +"%F %T UTC")" \
+  > $OPCSANDBOX/launch-info
+printf "with arguments: %s</p>" "$*"
+printf "with arguments: %s</p>" "$*" >>$OPCSANDBOX/launch-info
+opamcheck run -retries 2 -log-dir /log "$@"
+#rm -rf $OPCSANDBOX/summary
+while [ $# -gt 1 ]; do shift; done
+opamcheck summarize -log-dir /log -head "$(<$OPCSANDBOX/launch-info)" "$1"
