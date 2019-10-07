a778ae1f8f7d4a2a2d6e3ab7611c684a11b6dbe6
Florian Angeletti <florian.angeletti@inria.fr>
2019-10-07 16:12:33 +0200

    Fix depext install

--- external_deps_install.sh
+++ external_deps_install.sh
@@ -1,6 +1,23 @@
 opam update
+sudo apt update
+
+install () {
+sudo DEBIAN_FRONTEND=noninteractive apt install -y -q $1
+}
+
+query_and_install () {
+  l="$(opam list --readonly --external  --resolve=$1)"
+  if test -z "$l"
+  then
+    echo No dependencies
+  else
+	echo deps $l
+        install $l
+  fi
+}
+
 for i in $(opam list -a --columns=name | tail -n+3 )
 do
-	echo "installing $i";
-	opam depext --yes $i;
+	echo installing $i
+	query_and_install $i
 done;
